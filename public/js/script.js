c08a86439d6e2c7a53febf3de03e86579aa8a629
Patrick Weaver <patrick@mouse.org>
2016-09-22 14:54:27 -0400

    add all pages

---
+++ public/js/script.js
@@ -0,0 +1,38 @@
+angular.module('tools', [])
+	.controller('header', ['$scope', function($scope)
+	{
+		$scope.pages = [
+			{
+				"name": "About Badges",
+				"url": "/badges"
+			},
+			{
+				"name": "About Courses",
+				"url": "/courses"
+			},
+			{
+				"name": "Help",
+				"url": "/help"
+			},
+			{
+				"name": "Help: Accounts and Registration",
+				"url": "/help/accounts-and-registration"
+			},
+			{
+				"name": "Help: Badges and Credly",
+				"url": "help/badges-and-credly"
+			},
+			{
+				"name": "Help: Frequently Asked Questions",
+				"url": "/help/faq"
+			},
+			{
+				"name": "Help: Groups and Playlists",
+				"url": "help/groups-and-playlists"
+			},
+			{
+				"name": "Help: Projects",
+				"url": "help/projects"
+			}
+			]
+	}]);
