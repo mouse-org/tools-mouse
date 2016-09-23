09383c276b78076391e21b3d90573e59ec4f4171
patrickmouse <patrick@mouse.org>
2016-09-23 16:19:51 -0400

    update content

---
+++ public/js/data.js
@@ -0,0 +1,34 @@
+var pages_array = [
+			{
+				"name": "About Badges",
+				"url": "/badges"
+			},
+			{
+				"name": "Badge Directory",
+				"url": "/badge-directory"
+			},
+			{
+				"name": "About Courses",
+				"url": "/courses"
+			},
+			{
+				"name": "Help",
+				"url": "/help"
+			},
+			{
+				"name": "Help: Accounts and Registration",
+				"url": "/help/accounts-and-registration"
+			},
+			{
+				"name": "Help: Frequently Asked Questions",
+				"url": "/help/faq"
+			},
+			{
+				"name": "Help: Groups and Playlists",
+				"url": "/help/groups-and-playlists"
+			},
+			{
+				"name": "Help: Projects",
+				"url": "/help/projects"
+			}
+			]
