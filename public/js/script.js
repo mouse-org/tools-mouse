7bc3235c30f22a7ab6f1171b50e00a4ea2d1d9ee
Patrick Weaver <patrick@mouse.org>
2016-10-06 16:03:56 -0400

    angular standards first draft

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,49 @@
-angular.module('tools', [])
-	.controller('header', ['$scope', function($scope)
-	{
-		$scope.pages = pages_array;
-	}]);
+(function() {
+	var app = angular.module('tools', [ ]);
+
+	app.controller('HeaderController', function()
+		{
+			this.pages = pages_array;
+	});
+
+	app.controller('StandardsController', function() {
+			this.projects = projects;
+			this.standards = standards;
+
+			for (p in this.projects) {
+				project = this.projects[p];
+				project["display"] = true;
+				project["alignments"] = []
+				for(s in this.standards) {
+					standard = this.standards[s];
+					for(a in project.standards) {
+						standard_id = project.standards[a];
+						if (standard_id === standard.id){
+							project.alignments.push(standard);
+						}
+
+					}
+				}
+			}
+
+			this.select_all = true;
+			this.selection = [];
+
+			this.filterBy = function(selected) {
+				index = this.selection.indexOf(selected)
+				if (index === -1) {
+					this.selection.push(selected);
+					this.select_all = false;
+				} else {
+					this.selection.splice(index, 1);
+					if (this.selection.length === 0){
+						this.select_all = true;
+					}
+				}
+				
+			}
+
+
+	});
+		
+})();
