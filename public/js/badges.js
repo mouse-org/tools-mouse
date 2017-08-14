3508c5148b4dfc3d49124b0d72a581605016a781
Patrick <patrick@mouse.org>
2017-08-14 17:10:47 -0400

    update badges and courses to vue

---
+++ public/js/badges.js
@@ -0,0 +1,303 @@
+var badges = [
+  {
+    "badge_url": "https://credly.com/recipients/90502",
+    "badge_image": "/images/3Ddesign.png",
+    "projects": [
+      "Drawing in 3D",
+      "Basic Elements of 3D Shapes",
+      "Moving to the CAD World"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90105",
+    "badge_image": "/images/analogcircuits.png",
+    "projects": [
+      "Human Circuit",
+      "Light-Up Creature",
+      "Building with a Breadboard",
+      "Light Dimmers and Intelligent Sensors",
+      "Transistor Nightlight"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90093",
+    "badge_image": "/images/appssoftware.png",
+    "projects": [
+      "Software 101",
+      "Once Upon a Google Doc",
+      " Design a Mobile App",
+      "Build a Mobile App"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90106",
+    "badge_image": "/images/arduinocircuits_badge-01.png",
+    "projects": [
+      "Intro to the Arduino",
+      "LED Light Chaser",
+      "Arduino Sound Machine",
+      "Arduino Nightlight"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90477",
+    "badge_image": "/images/brainstorm_badge-01.png",
+    "projects": [
+      "Framing Questions",
+      "Rules of Brainstorming"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90113",
+    "badge_image": "/images/circuitdesign_badge-01.png",
+    "projects": [
+      "Design your Own Digital Circuit",
+      "Build your Own Digital Circuit"
+    ]
+  },  {
+    "badge_url": "https://credly.com/recipients/90098",
+    "badge_image": "/images/communicator_badge-01.png",
+    "projects": [
+      "Secret Sculpture",
+      "PSA: Be Professional"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90481",
+    "badge_image": "/images/empathy_badge-01.png",
+    "projects": [
+      "Design a Better World",
+      "Empathy and Assistive Tech"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90033",
+    "badge_image": "/images/fixer_badge-01.png",
+    "projects": [
+      "Help Desk Superhero",
+      "Setting Up A Ticket Tracking System",
+      "Reverse Engineering",
+      "Talk Like a Fixer",
+      "Do it Yourself Project"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90362",
+    "badge_image": "/images/gamedesigner.png",
+    "projects": [
+      "Intro to Game Design and Gamestar Mechanic",
+      "Core Mechanics",
+      "Goals, Obstacles & Chance",
+      "Found Object Game"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90500",
+    "badge_image": "/images/graphicdesign.png",
+    "projects": [
+      "Basic Elements of Graphic Design",
+      "Meet the Color Wheel",
+      "Pick a Palette",
+      "Designing Layout",
+      "Typography: Designing Fonts"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90040",
+    "badge_image": "/images/hardware.png",
+    "projects": [
+      "Ports & Connections",
+      "Hardware Go Fish",
+      "Hardware Everywhere",
+      "Internet of Things"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/86494",
+    "badge_image": "/images/htmlcss_badge-01.png",
+    "projects": [
+      "Tag Tag Revolution",
+      "Source Whisperer",
+      "Hack the News",
+      "Make a Simple Website with Thimble",
+      "HTML & CSS Relay"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90442",
+    "badge_image": "/images/hcd_badge-01.png",
+    "projects": [
+      "Design is Everywhere",
+      "Human Centered Design"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90492",
+    "badge_image": "/images/iteration_badge-01-01.png",
+    "projects": [
+      "Playtesting",
+      "Iterations & Feedback"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90027",
+    "badge_image": "/images/javascript_badge-01.png",
+    "projects": [
+      "Javascript Generator",
+      "Javascript Takeover",
+      "JavaScript Chatbot"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90365",
+    "badge_image": "/images/makingelectricity_badge-01.png",
+    "projects": [
+      "You’ve Got the Power",
+      " DIY Generator",
+      "Testing Turbines"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/89981",
+    "badge_image": "/images/networks.png",
+    "projects": [
+      "Ping Kong",
+      "Network Design Challenge",
+      "Say Yes to the IP Address",
+      "My Name Is URL"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90089",
+    "badge_image": "/images/operatingsystems.png",
+    "projects": [
+      "I is for Input",
+      " Learning Linux",
+      "OS for All",
+      "OS Design"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90493",
+    "badge_image": "/images/pitchandpublish_badge.png",
+    "projects": [
+      "Design Story"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90367",
+    "badge_image": "/images/poweringchange.png",
+    "projects": [
+      "Energy Sleuths",
+      "Become Action Heroes",
+      "Launch a Green Media Campaign",
+      "Pitch It"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90091",
+    "badge_image": "/images/programming_badge-01.png",
+    "projects": [
+      "People Programming",
+      "Text Your Computer with the Command Line",
+      "Hello, World: Programming  Languages"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90487",
+    "badge_image": "/images/prototyping_badge-01.png",
+    "projects": [
+      "Design in a Bag",
+      "Paper Prototyping"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90366",
+    "badge_image": "/images/renewablenergy.png",
+    "projects": [
+      "Hacking our Sustainable Future",
+      "Solarize It",
+      "Battery Builder"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90364",
+    "badge_image": "/images/scratchactivist_badge-01.png",
+    "projects": [
+      "The Serious Gamer",
+      "Grow-A- Game",
+      "Scratch Serious Game"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90363",
+    "badge_image": "/images/scratchdeveloper_badge-01.png",
+    "projects": [
+      "What is Scratch",
+      "Games From Scratch",
+      "It's The Remix"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90486",
+    "badge_image": "/images/synthesize_badge_4-01-01.png",
+    "projects": [
+      "Synthesizing Interviews",
+      "User Personas"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90096",
+    "badge_image": "/images/teambuilding_badge-01.png",
+    "projects": [
+      "The Mousemallow Challenge",
+      "Teamwork: The Mill Game",
+      "Leadership Wheel"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90473",
+    "badge_image": "/images/userinterviews_badge-01.png",
+    "projects": [
+      "Interview Strategies",
+      "Design Observation",
+      "Interviews with Target Users"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90509",
+    "badge_image": "/images/webcitizen.png",
+    "projects": [
+      "Creative Commons .GIF Exchange",
+      "Kracken the Code",
+      "Ad Battle"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/86497",
+    "badge_image": "/images/webprivacy.png",
+    "projects": [
+      "The Internet is a Live Mic",
+      "Awkward! Dealing with Strangers Online",
+      "Ring ToS",
+      "Social Network Sort"
+    ]
+  },
+  {
+    "badge_url": "https://credly.com/recipients/90022",
+    "badge_image": "/images/websecurity.png",
+    "projects": [
+      "Malware Avengers",
+      "Password Round Robin",
+      "Phish Market"
+    ]
+  }
+
+];
+
+var vueBadges = new Vue({
+  el: '#badges',
+  data: {
+    badges: badges
+  }
+});
---
+++ public/js/courses.js
@@ -0,0 +1,216 @@
+var courses = [
+
+
+  {
+    "course_name": "Circuitry & Electronics",
+    "course_description": "The Circuitry & Electronics course encourages teams to design, create, tinker, and build with technology. Participants build analog circuits with components like LEDs, transistors, and sensors. They program an Arduino microcontroller to create a digital music maker and light chaser. The course ends with participants designing and building their own digital circuit prototype.",
+    "course_overview": "https://drive.google.com/open?id=1hGSfULCeDobJB0NEPVPM3pmM-TTEhQpgpfQ92h62S5I",
+    "projects": [
+
+        "The Human Circuit",
+        "Light-Up Creature",
+        "Building with a Breadboard",
+        "Light Dimmers and Intelligent Sensors",
+        "Transistor Nightlight",
+        "Intro to the Arduino",
+        "LED Light Chaser",
+        "Arduino Sound Machine",
+        "Arduino Nightlight",
+        "Design your own Digital Circuit",
+        "Build your own Digital Circuit"
+    ]
+  },
+
+  {
+    "course_name": "Coding",
+    "course_description": "Mouse’s Coding course allows groups to get familiar with one of today’s most valuable 21st century skills. This course starts by focusing on front-end web design skills with activities on HTML, CSS and JavaScript. Learners then move to the command line where they get experience with a few languages and basic computer programming processes.",
+    "course_overview": "https://drive.google.com/open?id=1xUEF0R2fFrOKtHuVUL1S5XDe8R0cJpW8upk_MjzZ4MU",
+    "projects": [
+
+        "Tag Tag Revolution",
+        "Source Whisperer",
+        "Hack the News",
+        " Make a Simple Website with Thimble",
+        "HTML & CSS Relay",
+        "People Programming",
+        "Javascript Generator",
+        "Javascript Takeover",
+        "Javascript Chatbot",
+        "Text Your Computer: Use the Command Line",
+        "Hello, World: Programming Languages"
+    ]
+  },
+  {
+    "course_name": "Design with Purpose",
+    "course_description": "Groups working on the Design with Purpose course will research, brainstorm, prototype, and share their own technology innovation. By practicing and applying the Human-Centered Design process, youth will learn to empathize with their users and design technologies that address an authentic need.",
+    "course_overview": "https://docs.google.com/document/d/1qok8C7eVzlxxudqUH28B2SzBp1NnajI_LPb0w4TEhNs/edit?usp=sharing",
+    "projects": [
+
+        "Design is Everywhere",
+        "Human Centered Design",
+        "Design A Better World",
+        "Empathy and Assistive Tech",
+        "Interviewer Strategies",
+        "Design Observation",
+        "Interviews with Target Users",
+        "Synthesizing Interviews",
+        "User Personas",
+        "Framing Questions",
+        "Rules of Brainstorming",
+        "Design in a Bag",
+        "Paper Prototyping",
+        "Playtesting",
+        "Iterations & Feedback",
+        "Design Story"
+    ]
+  },
+  {
+    "course_name": "Green Tech",
+    "course_description": "The Green Tech course turns groups into sustainable technology leaders in their community. Youth build electricity generators, make batteries with everyday materials, and add solar panels to electric toys in order to better navigate the science behind eco-friendly technology. Youth will also experiment to improve the technology carbon footprint at their school and lead a action campaign with their findings.",
+    "course_overview": "https://docs.google.com/document/d/1T-sbWtmXJ6BFqc8vM366MiDywqNLuopqPh8rgTDbNFc/edit?usp=sharing",
+    "projects": [
+
+        "You've Got The Power",
+        "DIY Generator",
+        "Testing Turbines",
+        "Hacking for a Sustainable Future",
+        "Solarize It",
+        "Battery Builder",
+        "Energy Sleuths",
+        "Become Action Heroes",
+        "Launch a Green Media Campaign",
+        "Pitch It"
+    ]
+  },
+  {
+    "course_name": "Intro to Design",
+    "course_description": "The Intro to Design course gives youth the skills they need to begin creating original graphics, visual displays, animations, and models. Participants learn the essential elements to consider when engaging in graphic and 3D design. They use color wheels to choose color schemes, practice design layout techniques, and create an original typeface. They also learn how to draw shapes and objects in 3D and to create models in 3D CAD (computer-assisted design) software.",
+    "course_overview": "https://docs.google.com/document/d/1VM_jE66-7zlee1JfpnE6KhcaBvWzCHWZoLiwFPke_Sc/edit?usp=sharing",
+    "projects": [
+
+        "Basic Elements of Graphic Design",
+        "Meet the Color Wheel",
+        "Pick a Palette",
+        "Designing Layout",
+        "Typography: Designing Fonts",
+        "Basic Elements of 3D Shapes",
+        "Drawing in 3D",
+        "Moving to the CAD World",
+        "Model Something Real in CAD"
+    ]
+  },
+  {
+    "course_name": "IT Admin",
+    "course_description": "The IT Admin course is a follow-up to the IT Essentials course. Youth learn the structure and design of networks and the internet while exploring interfaces through peripherals, operating systems and app design.",
+    "course_overview": "https://drive.google.com/open?id=1qME0PvO_rZ1LycncbGohyYKsATjEi8uvxEZwlBfOaXo",
+    "projects": [
+
+        "Ping Kong",
+        "Network Design Challenge",
+        "Say Yes to the IP Address",
+        " I is for Input",
+        "My Name Is URL",
+        "Learning Linux",
+        "Design a Mobile App",
+        "Build a Mobile App with App Inventor"
+    ]
+  },
+  {
+    "course_name": "IT Essentials",
+    "course_description": "The IT Essentials course is an introduction to working with technology in hands-on and innovative ways. Youth start by exploring computer hardware and software, then build on these skills by designing new ways of interacting with technology.",
+    "course_overview": "https://drive.google.com/open?id=1Ps1L2faNbLbQ3-OcEESA8UBw2l4e77naC5flq23eRgM",
+    "projects": [
+
+        "Ports & Connections",
+        "Hardware Go Fish",
+        "Hardware Everywhere",
+        " Software 101",
+        "Once Upon a Google Doc",
+        "OS for All",
+        "OS Design",
+        "Internet of Things"
+    ]
+  },
+
+  {
+    "course_name": "Serious Games",
+    "course_description": "The Serious Game Design course introduces youth to the building blocks of game design and the world of socially responsible gaming. Participants then research, design, and code a game prototype in Scratch.",
+    "course_overview": "https://drive.google.com/open?id=1mVoQv0tI9mL8S8UEtMXgqdlG4MrO61S1qlOAUhzoBTM",
+    "projects": [
+
+        "Intro to Game Design and Gamestar Mechanic",
+        "Core Mechanics",
+        "Goals, Obstacles & Chance",
+        "Found Object Game",
+        "What is Scratch?",
+        "Games from Scratch",
+        "It’s the Remix",
+        "The Serious Gamer",
+        "Grow-A-Game",
+        "Scratch Serious Game"
+    ]
+  },
+
+  {
+    "course_name": "Sewable Tech",
+    "course_description": "Sewable Tech combines STEM learning with artistic expression and crafting. Research shows that learning circuitry through this STEAM lens engages a wider audience than traditional circuit design and improves learner outcomes! In this 9-project course, students will learn to embed circuitry directly into fabric-- creating soft circuits, fabric switches and sewable sensors. The course will culminate in a final project that students will sew and program to help them set and achieve goals.",
+    "course_overview": "https://docs.google.com/document/d/1tvBUnoM41YrMTq0LbSqz_WUpa0Ww1PG8XTToeKMw37Y",
+    "projects": [
+
+        "Sew a Tech Case",
+        "Sew a Basic Circuit",
+        "Sewable Superhero Cuff",
+        "Sew an LED Snap Circuit",
+        "Fabric Push Button",
+        "DIY Bend Sensor",
+        "RGB Color Mixer",
+        "Intro to LilyPad Arduino USB",
+        "LilyPad Activity Tracker"
+    ]
+  },
+
+  {
+    "course_name": "Tech Team",
+    "course_description": "The Tech Team course is for groups looking to provide technical and user support at their site as a help desk. In addition to completing both IT courses, Tech Team groups participate in team building activities, build troubleshooting skills, develop routines and professional communication, and give back through community service.",
+    "course_overview": "https://drive.google.com/open?id=1OrfPN11uNAlUt42tYOX83MKo6WHpEPcqgfDkdFkxcTU",
+    "projects": [
+
+        "The Mousemallow Challenge",
+        "Teamwork: The Mill Game",
+        "Help Desk Superhero",
+        "Setting Up A Ticket Tracking System",
+        "Reverse Engineering",
+        "Secret Sculpture",
+        "Talk Like a Fixer",
+        "PSA: Be Professional",
+        "Leadership Wheel",
+        "Do It Yourself Project"
+    ]
+  },
+  {
+    "course_name": "Web Literacy",
+    "course_description": "Mouse’s Web Literacy course is all about transforming youth from casual web users to savvy participants who can think critically, participate safely, and demonstrate good citizenship online. Groups will learn about data on the web as it relates to users and privacy, develop the knowledge and skills needed to keep safe online, and explore the issues of policy, choice, and research on the web. ",
+    "course_overview": "https://drive.google.com/open?id=1rQR5gSp9C2HZtfrSQPluMv_HB5BtpduGG349kbnYkc0",
+    "projects": [
+
+        "The Internet is a Live Mic",
+        "Creative Commons .GIF Exchange",
+        "Kracken the Code",
+        "Ad Battle",
+        "Awkward! Dealing with Strangers Online",
+        "Paper ToS",
+        "Social Network Sort",
+        "Malware Avengers",
+        "Password Round Robin",
+        "Phish Market"
+    ]
+  }
+]
+
+
+var vueCourses = new Vue({
+  el: '#courses',
+  data: {
+    courses: courses
+  }
+});
