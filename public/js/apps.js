bf808393ba365a8a331d7b96452d6277ddfe0179
Patrick <patrick@mouse.org>
2017-11-21 14:35:16 -0500

    add app to menu

--- public/js/apps.js
+++ public/js/apps.js
@@ -5,6 +5,11 @@ var apps = [
     "name": "Social Network Sort",
     "thumbnail": "/images/apps/socialNetworkSort.png",
     "url": "social-network-sort"
+  },
+  {
+    "name": "Hacking Smogville's Sustainable Future",
+    "thumbnail": "/images/apps/smogville.png",
+    "url": "hacking-smogvilles-future"
   }
 ]
 
