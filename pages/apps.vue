491ff2aaa5a705a43c5af940823c23dc6764413c
Patrick Weaver <pjpwea@gmail.com>
2019-01-15 11:15:02 -0500

    move apps page to nuxt

---
+++ pages/apps.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <Menu />
+    <div v-html="md" />
+
+    <div id="apps">
+      <div
+        v-for="app in apps"
+        :key="app.url"
+        class="app-thumb"
+      >
+        <a
+          :href="app.url"
+          target="_blank"
+        >
+          <img :src="app.thumbnail">
+
+          <div class="app-name">
+            {{ app.name }}
+          </div>
+        </a>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Menu from '~/components/Menu.vue'
+import md from '~/markdownPages/apps.md'
+import apps from '~/json/apps.json'
+
+export default {
+  components: {
+    Menu
+  },
+  data() {
+    return {
+      apps
+    }
+  },
+  computed: {
+    md() {
+      return md
+    }
+  }
+}
+</script>
