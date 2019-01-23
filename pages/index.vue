69744eab109e163a291daf5e695734b4b99987d8
pzyuan <35542448+pzyuan@users.noreply.github.com>
2019-01-23 14:35:47 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,24 @@
 <template>
   <section class="container">
     <div>
-
       <Menu />
-
-      <h1>
-        About Mouse Create
-      </h1>
-      <p>Mouse Create is currently in Beta. We are working hard to iterate on the experience for both Educators and Youth and appreciate any feedback you have. You can send questions, comments, or ideas to help@mouse.org.</p>
-
-
-      <p>If you don't see the answer to your question here, or have a question specific to your site please email <a href="mailto:help@mouse.org">help@mouse.org</a>.</p>
+      <div v-html="md" />
     </div>
   </section>
 </template>
 
 <script>
 import Menu from '~/components/Menu.vue'
+import md from '~/assets/md/index.md'
 
 export default {
   components: {
     Menu
+  },
+  computed:{
+    md(){
+    return md
+    }
   }
 }
 </script>
