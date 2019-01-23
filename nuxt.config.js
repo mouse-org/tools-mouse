cdadf058cda958e0280a1cbcc6efeeb4ad16805f
pzyuan <35542448+pzyuan@users.noreply.github.com>
2019-01-23 14:40:21 -0500

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Mouse Create Tools &amp; Help',
+    title: 'Mouse Create Tools & Help',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
