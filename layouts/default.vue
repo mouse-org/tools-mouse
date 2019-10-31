8bf39f039b4b1cdcafdec6a870740cfa9a9f14a3
Patrick Weaver <pjpwea@gmail.com>
2019-10-31 09:56:31 -0400

    re-add Google Analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,7 +16,7 @@ img {
 */
 export default {
   created() {
-    /*
+    
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -24,7 +24,7 @@ export default {
 
     ga('create', 'UA-84363943-1', 'auto')
     ga('send', 'pageview')
-    */
+    
   }
 }
 </script>
